ce49b1f4ba126b7f3a468730e1be87be49256634
Arkye <arkye@hotmail.com.br>
2016-02-18 07:06:46 -0500

    Exercises: Promotions

--- exercises/my_chocolate_company/js/displayPromotion.js
+++ exercises/my_chocolate_company/js/displayPromotion.js
@@ -10,33 +10,31 @@ function showCurrentPromotion(f) {
 	if (currentMonth == 2) {
 		if (currentDay == 9) {
 			// Fat Tuesday Sale Promotion
-			document.getElementById("text_promotion").innerHTML = "Fat Tuesday Sale! Promotion will be available until the end of today (February 09).";
-			document.getElementById("current_promotion").innerHTML = "<img src='img/promotion_0209.png'>";
+			document.getElementById("text_promotion").innerHTML = "<p> Fat Tuesday Sale! Promotion will be available until the end of today (February 09).";
+			document.getElementById("current_promotion").innerHTML = "<img src='img/promotion_0209.png' width='570px' height='190px'>";
 		}
 		else if (currentDay == 14) {
 			// Valentine's Day Promotion
-			document.getElementById("text_promotion").innerHTML = "Valentine's Day Promotion will be available until the end of today (February 14).";
-			document.getElementById("current_promotion").innerHTML = "<img src='img/promotion_0214.png'>";
+			document.getElementById("text_promotion").innerHTML = "<p> Valentine's Day Promotion will be available until the end of today (February 14).";
+			document.getElementById("current_promotion").innerHTML = "<img src='img/promotion_0214.png' width='570px' height='190px'>";
 		}
 		else if (currentDay == 29) {
 			// Leap Day Sale Promotion
-			document.getElementById("text_promotion").innerHTML = "Leap Day Sale! Promotion will be available until the end of today (February 29).";
-			document.getElementById("current_promotion").innerHTML = "<img src='img/promotion_0229.png'>";
+			document.getElementById("text_promotion").innerHTML = "<p> Leap Day Sale! Promotion will be available until the end of today (February 29).";
+			document.getElementById("current_promotion").innerHTML = "<img src='img/promotion_0229.png' width='570px' height='190px'>";
 		}
 		else {
 			// No Current Promotion
-			document.getElementById("text_promotion").innerHTML = "There's no promotion today. Check again another time!";
-			document.getElementById("current_promotion").innerHTML = "<img src='img/no_current_promotion.png'>";
+			document.getElementById("text_promotion").innerHTML = "<p> There's no promotion today. Check again another time!";
 		}
 	}
 	else if (currentMonth == 3) {
 		// March Madness Sale Promotion
-		document.getElementById("text_promotion").innerHTML = "Mach Madness Sale will be available during the entire month!";
-		document.getElementById("current_promotion").innerHTML = "<img src='img/promotion_03.png'>";
+		document.getElementById("text_promotion").innerHTML = "<p> March Madness Sale will be available during the entire month!";
+		document.getElementById("current_promotion").innerHTML = "<img src='img/promotion_03.png' width='570px' height='190px'>";
 	}
 	else {
 		// No Current Promotion
-		document.getElementById("text_promotion").innerHTML = "There's no promotion today. Check again another time!";
-		document.getElementById("current_promotion").innerHTML = "<img src='img/no_current_promotion.png'>";
+		document.getElementById("text_promotion").innerHTML = "<p> There's no promotion today. Check again another time!";
 	}
 }
