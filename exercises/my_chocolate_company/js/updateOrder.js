aba436138c5cce696be60a1194d8040581d66ad1
Arkye <arkye@hotmail.com.br>
2016-03-03 03:56:57 -0500

    Update OrderNow Page

--- exercises/my_chocolate_company/js/updateOrder.js
+++ exercises/my_chocolate_company/js/updateOrder.js
@@ -1,4 +1,11 @@
+function checkoutOrder(f) {
+
+	totalValue = document.getElementById("total").value.toString();
+	alert("You purchased $" + totalValue + " of chocolate! Have a sweet day! :)");
+}
+
 function updateOrder(f) {
+
 	CHERRY_CHOC_VALUE = 2.50;
 	GLUTTONY_VALUE = 4.00;
 	LOVE_VALUE = 3.00;
@@ -14,21 +21,133 @@ function updateOrder(f) {
 	loveTotal = LOVE_VALUE*loveQuantity;
 	spamTotal = SPAM_VALUE*spamQuantity;
 
-	document.getElementById("cherrychoc_total").innerHTML = "$" + cherryChocTotal.toFixed(2);
-	document.getElementById("gluttony_total").innerHTML = "$" + gluttonyTotal.toFixed(2);
-	document.getElementById("love_total").innerHTML = "$" + loveTotal.toFixed(2);
-	document.getElementById("spam_total").innerHTML = "$" + spamTotal.toFixed(2);
+	document.getElementById("cherrychoc_total").innerHTML = cherryChocTotal.toFixed(2);
+	document.getElementById("gluttony_total").innerHTML = gluttonyTotal.toFixed(2);
+	document.getElementById("love_total").innerHTML = loveTotal.toFixed(2);
+	document.getElementById("spam_total").innerHTML = spamTotal.toFixed(2);
 
 	subTotal = cherryChocTotal + gluttonyTotal + loveTotal + spamTotal;
 
-	document.getElementById("subtotal").innerHTML = "$" + subTotal.toFixed(2);
+	document.getElementById("subtotal").innerHTML = subTotal.toFixed(2);
 
 	TAX_VALUE = 0.07;
 	tax = subTotal*TAX_VALUE;
 
-	document.getElementById("tax").innerHTML = "$" + tax.toFixed(2);
+	document.getElementById("tax").innerHTML = tax.toFixed(2);
 
 	total = subTotal + tax;
 
-	document.getElementById("total").innerHTML = "$" + total.toFixed(2);
+	document.getElementById("total").innerHTML = total.toFixed(2);
+}
+
+function decreaseCherryChocQuantity(f) {
+
+	cherryChocQuantity = document.getElementById("cherrychoc_quantity").value;
+
+	if (cherryChocQuantity <= 0) {
+		document.getElementById("cherrychoc_quantity").innerHTML = 0;
+	} else {
+		cherryChocQuantity--;
+		document.getElementById("cherrychoc_quantity").innerHTML = cherryChocQuantity;
+	}
+
+	updateOrder(f);
+}
+
+function increaseCherryChocQuantity(f) {
+
+	cherryChocQuantity = document.getElementById("cherrychoc_quantity").value;
+
+	if (cherryChocQuantity >= 99) {
+		document.getElementById("cherrychoc_quantity").innerHTML = 99;
+	} else {
+		cherryChocQuantity++;
+		document.getElementById("cherrychoc_quantity").innerHTML = cherryChocQuantity;
+	}
+
+	updateOrder(f);
+}
+
+function decreaseGluttonyQuantity(f) {
+
+	gluttonyQuantity = document.getElementById("gluttony_quantity").value;
+
+	if (gluttonyQuantity <= 0) {
+		document.getElementById("gluttony_quantity").innerHTML = 0;
+	} else {
+		gluttonyQuantity--;
+		document.getElementById("gluttony_quantity").innerHTML = gluttonyQuantity;
+	}
+
+	updateOrder(f);
+}
+
+function increaseGluttonyQuantity(f) {
+
+	gluttonyQuantity = document.getElementById("gluttony_quantity").value;
+
+	if (gluttonyQuantity >= 99) {
+		document.getElementById("gluttony_quantity").innerHTML = 99;
+	} else {
+		gluttonyQuantity++;
+		document.getElementById("gluttony_quantity").innerHTML = gluttonyQuantity;
+	}
+
+	updateOrder(f);
+}
+
+function decreaseLoveQuantity(f) {
+
+	loveQuantity = document.getElementById("love_quantity").value;
+
+	if (loveQuantity <= 0) {
+		document.getElementById("love_quantity").innerHTML = 0;
+	} else {
+		loveQuantity--;
+		document.getElementById("love_quantity").innerHTML = loveQuantity;
+	}
+
+	updateOrder(f);
+}
+
+function increaseLoveQuantity(f) {
+
+	loveQuantity = document.getElementById("love_quantity").value;
+
+	if (loveQuantity >= 99) {
+		document.getElementById("love_quantity").innerHTML = 99;
+	} else {
+		loveQuantity++;
+		document.getElementById("love_quantity").innerHTML = loveQuantity;
+	}
+
+	updateOrder(f);
+}
+
+function decreaseSpamQuantity(f) {
+
+	spamQuantity = document.getElementById("spam_quantity").value;
+
+	if (spamQuantity <= 0) {
+		document.getElementById("spam_quantity").innerHTML = 0;
+	} else {
+		spamQuantity--;
+		document.getElementById("spam_quantity").innerHTML = spamQuantity;
+	}
+
+	updateOrder(f);
+}
+
+function increaseSpamQuantity(f) {
+
+	spamQuantity = document.getElementById("spam_quantity").value;
+
+	if (spamQuantity >= 99) {
+		document.getElementById("spam_quantity").innerHTML = 99;
+	} else {
+		spamQuantity++;
+		document.getElementById("spam_quantity").innerHTML = spamQuantity;
+	}
+
+	updateOrder(f);
 }
